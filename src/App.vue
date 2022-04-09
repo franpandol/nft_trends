593abd3dcb4c60e4f1542035d15aa4160941f71b
FranPandol <pandol.francisco@gmail.com>
2022-04-09 20:20:36 -0300

    List NFTs from ETH Addresses using alchemy & axios

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+Number of NFTS: {{ nfts_length }}<br />
+Owner: {{ owner_address }}
+<div style="padding: 40px" class="col-12" v-for="nft in nfts" v-bind:key="nft.id">
+    <h3>{{ nft.title }}</h3>
+    <p>{{ nft.description }}</p>
+    <img :src="nft.metadata.image" />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    data() {
+        return {
+            nfts: [],
+            nfts_length: null,
+            owner_address: null,
+        };
+    },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    methods: {
+        async getData() {
+            // replace with your own address
+            var owner = "<Ethereum address>";
+            // construct the axios request:
+            var config = {
+                method: "get",
+                url: "https://eth-mainnet.alchemyapi.io/v2/" +
+                    process.env.VUE_APP_ALCHEMY_API_KEY +
+                    "/getNFTs/?owner=" +
+                    owner,
+            };
+            // make the request and print the formatted response:
+            axios(config)
+                .then((response) => {
+                    this.nfts = response.data.ownedNfts;
+                    this.nfts_length = this.nfts.length;
+                    this.owner_address = owner;
+
+					for (const nft in this.nfts) {
+						// replace ipfs with a public ipfs node to see the images
+                        this.nfts[nft].metadata.image =
+                            "https://gateway.pinata.cloud/ipfs/" +
+                            this.nfts[nft].metadata.image.replace("ipfs://", "");
+                    }
+                })
+                .catch((error) => console.log(error));
+        },
+    },
+
+    created() {
+        this.getData();
+    },
+};
+</script>
